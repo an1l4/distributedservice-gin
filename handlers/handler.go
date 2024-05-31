8c86fa385f317ba7b1d3c2adc55184fa4b9603ef
an1l4 <1995anila@gmail.com>
2024-05-31 17:20:13 +0530

    api key implementation

--- handlers/handler.go
+++ handlers/handler.go
@@ -87,6 +87,10 @@ func (handler *RecipesHandler) ListRecipesHandler(c *gin.Context) {
 //	'400':
 //	    description: Invalid input
 func (handler *RecipesHandler) NewRecipeHandler(c *gin.Context) {
+	/*if c.GetHeader("X-API-KEY") != os.Getenv("X_API_KEY") {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "API key not provided or invalid"})
+		return
+	}*/
 	var recipe models.Recipe
 	if err := c.ShouldBindJSON(&recipe); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
